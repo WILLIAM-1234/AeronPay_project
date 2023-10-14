7016f4ef57c6f0234550ff88bb552ddc67f394f0
WILLIAM CAREY MARLAPUDI <77280705+WILLIAM-1234@users.noreply.github.com>
2023-10-15 03:30:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code here
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone');
+    const subjectInput = document.getElementById('subject');
+    const messageInput = document.getElementById('message');
+    // ... (the rest of your code)
+    
+    // Add event listeners
+    nameInput.addEventListener('input', function() {
+        console.log('Name: ' + nameInput.value);
+        console.log('Email: ' + emailInputInput.value);
+        console.log('Phone: ' + phoneInput.value);
+        console.log('Subject: ' + subjectInput.value);
+        console.log('Message: ' + messageInput.value);
+    });
+    nameInput.addEventListener('input', logValue);
+    nameInput.addEventListener('change', logValue);
+
+    emailInput.addEventListener('input', logValue);
+    emailInput.addEventListener('change', logValue);
+
+    phoneInput.addEventListener('input', logValue);
+    phoneInput.addEventListener('change', logValue);
+
+    subjectInput.addEventListener('input', logValue);
+    subjectInput.addEventListener('change', logValue);
+
+    messageInput.addEventListener('input', logValue);
+    messageInput.addEventListener('change', logValue);
+
+    function logValue(event) {
+        console.log(event.target.id + ': ' + event.target.value);
+    }
+});
